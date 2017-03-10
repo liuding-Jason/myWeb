03975ca44d29002f4deb42b31423f217f39ae2ba
liuding <liuding@ifmuse.com>
2017-03-10 15:50:18 +0800

    add music component

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app" >
+      <ld-music />
       <transition name="path-fade" mode="out-in">
         <!-- 路由匹配到的组件将渲染在这里 -->
         <router-view></router-view>
@@ -9,12 +10,14 @@
 
 <script>
 
+import ldMusic from "./components/ld-music.vue" ;
 const SetDpr = require("./util/dpr.js") ; 
 let sd = new SetDpr() ;
 
 export default {
   name: 'app',
   components: {
+    ldMusic
   } ,
   methods : {
     _onResize : function(){
@@ -44,6 +47,8 @@ body{
   background-size : cover ;
   height : 100% ;
   width : 100%;
+  position : relative ;
+
   
   -webkit-transition: All 2s ease;
   -moz-transition: All 2s ease;
@@ -96,46 +101,6 @@ id{
   -o-align-items: center;
   -webkit-box-align: center;
   -webkit-align-items: center;
-}
-
-@keyframes myfirst
-{
-  from{
-    height : 0 ;
-  }
-  to {
-    height : 50% ;
-  }
-}
-
-@-moz-keyframes myfirst /* Firefox */
-{
-  from{
-    height : 0 ;
-  }
-  to {
-    height : 50% ;
-  }
-}
-
-@-webkit-keyframes myfirst /* Safari 和 Chrome */
-{
-  from{
-    height : 0 ;
-  }
-  to {
-    height : 50% ;
-  }
-}
-
-@-o-keyframes myfirst /* Opera */
-{
-  from{
-    height : 0 ;
-  }
-  to {
-    height : 50% ;
-  }
 }
 
 .path-fade-enter-active, .path-fade-leave-active {
--- src/components/ld-music.vue
+++ src/components/ld-music.vue
@@ -1,6 +1,6 @@
 <template>
-	<div>
-		<audio> </audio>
+	<div class="mod-music" title="change song" v-on:click="changeSong">
+		<audio id="music-play" src="" loop autoplay ></audio>
 	</div>
 </template>
 
@@ -11,22 +11,9 @@
 	export default {
 		data () {
 			return {
-				list : [
-					{
-						name : 'Jack' ,
-						age	 : 15 ,
-						score : 87
-					} ,
-					{
-						name : 'Tom' ,
-						age : 14 ,
-						score : 90
-					} ,
-					{
-						name : 'Rose' ,
-						age : 16 ,
-						score : 100
-					}
+				play : true ,
+				songs : [
+					
 				]
 			}
 		} ,
@@ -35,7 +22,7 @@
 
 		methods : {
 			hello : function(){
-				console.log("hello list!");
+				console.log("hello music!");
 			} ,
 			judgeData : function(code = void 0 , message = ""){
 				if(code * 1 !== 0){
@@ -43,6 +30,19 @@
 					return false ;
 				}
 				return true ;
+			} ,
+			changeSong : function(){
+				if(!this.play) return ;
+				this.play = false ;
+				let time = setTimeout(() => {
+					let t  = Math.floor(Math.random() * this.songs.length);
+					let mp = document.getElementById("music-play") ;
+					mp.src = this.songs[t] ;
+
+					this.play = true ;
+					clearTimeout(time);
+					console.log("change song");
+				} , 300);
 			}
 		} ,
 
@@ -64,13 +64,149 @@
 			//});
 
 			this.hello();
-
-
 		}
 
 	}
 </script>
 
 <style scoped>
+.mod-music {
+	position : absolute ;
+	height : .6rem ;
+	width : .6rem ;
+	top : 10px;
+	right : 15px ;
+	background : url(../img/music.png) no-repeat ;
+	background-size : cover ;
+	cursor : pointer ;
+
+	animation: myfirst 5s linear 2s infinite ;
+	/* Firefox: */
+	-moz-animation: myfirst 5s linear 2s infinite ;
+	/* Safari 和 Chrome: */
+	-webkit-animation: myfirst 5s linear 2s infinite ;
+	/* Opera: */
+	-o-animation: myfirst 5s linear 2s infinite ;
+}
+
+.mod-music:active{
+	border : 1px solid #fbfbfb ;
+}
+
+@keyframes myfirst
+{
+  0% {
+  	transform: rotateY(0deg);
+	-webkit-transform: rotateY(0deg);	/* Safari 和 Chrome */
+	-moz-transform: rotateY(0deg);	/* Firefox */
+  }
+  25% {
+  	transform: rotateY(90deg);
+	-webkit-transform: rotateY(90deg);	/* Safari 和 Chrome */
+	-moz-transform: rotateY(90deg);	/* Firefox */
+  }
+  50%{
+  	transform: rotateY(180deg);
+	-webkit-transform: rotateY(180deg);	/* Safari 和 Chrome */
+	-moz-transform: rotateY(180deg);	/* Firefox */
+  }
+  75%{
+  	transform: rotateY(270deg);
+	-webkit-transform: rotateY(270deg);	/* Safari 和 Chrome */
+	-moz-transform: rotateY(270deg);	/* Firefox */
+  }
+  100%{
+  	transform: rotateY(360deg);
+	-webkit-transform: rotateY(360deg);	/* Safari 和 Chrome */
+	-moz-transform: rotateY(360deg);	/* Firefox */
+  }
+}
+
+@-moz-keyframes myfirst /* Firefox */
+{
+  0% {
+  	transform: rotateY(0deg);
+	-webkit-transform: rotateY(0deg);	/* Safari 和 Chrome */
+	-moz-transform: rotateY(0deg);	/* Firefox */
+  }
+  25% {
+  	transform: rotateY(90deg);
+	-webkit-transform: rotateY(90deg);	/* Safari 和 Chrome */
+	-moz-transform: rotateY(90deg);	/* Firefox */
+  }
+  50%{
+  	transform: rotateY(180deg);
+	-webkit-transform: rotateY(180deg);	/* Safari 和 Chrome */
+	-moz-transform: rotateY(180deg);	/* Firefox */
+  }
+  75%{
+  	transform: rotateY(270deg);
+	-webkit-transform: rotateY(270deg);	/* Safari 和 Chrome */
+	-moz-transform: rotateY(270deg);	/* Firefox */
+  }
+  100%{
+  	transform: rotateY(360deg);
+	-webkit-transform: rotateY(360deg);	/* Safari 和 Chrome */
+	-moz-transform: rotateY(360deg);	/* Firefox */
+  }
+}
+
+@-webkit-keyframes myfirst /* Safari 和 Chrome */
+{
+  0% {
+  	transform: rotateY(0deg);
+	-webkit-transform: rotateY(0deg);	/* Safari 和 Chrome */
+	-moz-transform: rotateY(0deg);	/* Firefox */
+  }
+  25% {
+  	transform: rotateY(90deg);
+	-webkit-transform: rotateY(90deg);	/* Safari 和 Chrome */
+	-moz-transform: rotateY(90deg);	/* Firefox */
+  }
+  50%{
+  	transform: rotateY(180deg);
+	-webkit-transform: rotateY(180deg);	/* Safari 和 Chrome */
+	-moz-transform: rotateY(180deg);	/* Firefox */
+  }
+  75%{
+  	transform: rotateY(270deg);
+	-webkit-transform: rotateY(270deg);	/* Safari 和 Chrome */
+	-moz-transform: rotateY(270deg);	/* Firefox */
+  }
+  100%{
+  	transform: rotateY(360deg);
+	-webkit-transform: rotateY(360deg);	/* Safari 和 Chrome */
+	-moz-transform: rotateY(360deg);	/* Firefox */
+  }
+}
+
+@-o-keyframes myfirst /* Opera */
+{
+  0% {
+  	transform: rotateY(0deg);
+	-webkit-transform: rotateY(0deg);	/* Safari 和 Chrome */
+	-moz-transform: rotateY(0deg);	/* Firefox */
+  }
+  25% {
+  	transform: rotateY(90deg);
+	-webkit-transform: rotateY(90deg);	/* Safari 和 Chrome */
+	-moz-transform: rotateY(90deg);	/* Firefox */
+  }
+  50%{
+  	transform: rotateY(180deg);
+	-webkit-transform: rotateY(180deg);	/* Safari 和 Chrome */
+	-moz-transform: rotateY(180deg);	/* Firefox */
+  }
+  75%{
+  	transform: rotateY(270deg);
+	-webkit-transform: rotateY(270deg);	/* Safari 和 Chrome */
+	-moz-transform: rotateY(270deg);	/* Firefox */
+  }
+  100%{
+  	transform: rotateY(360deg);
+	-webkit-transform: rotateY(360deg);	/* Safari 和 Chrome */
+	-moz-transform: rotateY(360deg);	/* Firefox */
+  }
+}
 
 </style>
